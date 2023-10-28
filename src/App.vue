d0bb76ea3d4051a989a3ed6c27e128ed58a10825
wilsonuofa <wvtruong@ualberta.ca>
2023-10-28 12:18:18 -0600

    <GoogleMap api-key="AIzaSyDzhSt4jyXKEkk5GRBUkNBaeRmoPIzbAPo" style="width: 100%; height: 500px" :center="center" :zoom="15">     <Marker :options="{ position: center }" />   </GoogleMap> </template>
    <script>
    import { defineComponent } from "vue";
    import { GoogleMap, Marker } from "vue3-google-map";
    
    export default defineComponent({
      components: { GoogleMap, Marker },
      setup() {
        const center = { lat: 40.689247, lng: -74.044502 };
    
        return { center };
      },
    });
    </script> Please enter the commit message for your changes. Lines starting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import UserLocation from './components/UserLocation.vue'
+import Map from './components/Map.vue'
 </script>
 
 <template>
-    <UserLocation></UserLocation>
+    <Map></Map>
+    Hello
 </template>
 
 <style scoped></style>
