aef6b5756758b2bf2fdeab0a7630daeed1ee7af3
Eric Xiong <exiong@ualberta.ca>
2023-10-28 14:11:04 -0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
 import UserLocation from './components/UserLocation.vue'
 import Map from './components/Map.vue'
+
+const t = [1, 2, 3]
+const fetchEvents = async() =>{
+    const clientID = "yJwWTL9ZIxBdwB74S77pX"
+    const clientSecret = "rzE1z8DJhQtmuDzJCNKyjCoBbOtZ27LpfBl8OHOg"
+    const res = await fetch('https://api.aerisapi.com/fires/within?p=edmonton,ab&format=json&client_id=yJwWTL9ZIxBdwB74S77pX&client_secret=rzE1z8DJhQtmuDzJCNKyjCoBbOtZ27LpfBl8OHOg&limit=250&radius=1000km')
+    return await res.json()
+}
+const events = fetchEvents();
+console.log(events)
+
 </script>
 
 <template>
-    <Map></Map>
-    Hello
+    <Map>
+        <div v-for="event in events" :lat="event.loc.lat" :lng="event.loc.long">HELLO</div>
+    </Map>
 </template>
 
-<style scoped></style>
+<style>
+#app{
+    width: 100vw;
+    height: 100vh;
+    padding: 0 !important;
+    margin: 0 !important;
+    max-width: none;
+}
+</style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,17 +1,26 @@
 <template>
-  <GoogleMap api-key="AIzaSyDzhSt4jyXKEkk5GRBUkNBaeRmoPIzbAPo" style="width: 100%; height: 500px" :center="center" :zoom="15">
-    <Marker :options="{ position: center }" />
+  <GoogleMap api-key="AIzaSyDzhSt4jyXKEkk5GRBUkNBaeRmoPIzbAPo" style="width: 100%; height: 100%" :center="center" :zoom="10">
+    <Marker v-for="event in events" :options="{ position: {lat: event.loc.lat, lng: event.loc.long} }" />
   </GoogleMap>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 import { GoogleMap, Marker } from "vue3-google-map";
+const fetchEvents = async() =>{
+    const clientID = "yJwWTL9ZIxBdwB74S77pX"
+    const clientSecret = "rzE1z8DJhQtmuDzJCNKyjCoBbOtZ27LpfBl8OHOg"
+    const res = await fetch('https://api.aerisapi.com/fires/within?p=edmonton,ab&format=json&client_id=yJwWTL9ZIxBdwB74S77pX&client_secret=rzE1z8DJhQtmuDzJCNKyjCoBbOtZ27LpfBl8OHOg&limit=250&radius=1000km')
+    return await res.json()
+}
+const events = fetchEvents();
+
 
 export default defineComponent({
   components: { GoogleMap, Marker },
   setup() {
-    const center = { lat: 40.689247, lng: -74.044502 };
+
+    const center = { lat: 53.5232, lng: -113.18 }
 
     return { center };
   },
