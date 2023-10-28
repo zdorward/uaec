7d17e37a9e91ed279b9e8b90db4f4cc9f13a6d6c
Zack Dorward <zdorward1@gmail.com>
2023-10-28 12:00:30 -0600

    worked on user location dropdown

--- src/components/UserLocation.vue
+++ src/components/UserLocation.vue
@@ -1,31 +1,66 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { getCoords } from '../composables/location'
+import { city_names } from '../data/cities'
+
+const location = ref('Edmonton')
+const showButtons = ref(true)
+const enterLocation = ref(false)
 
 const getUserLocation = () => {
     console.log('test')
     getCoords()
+    showButtons.value = false
 }
 
-const count = ref(0)
+const showEnterLocation = () => {
+    enterLocation.value = true
+    showButtons.value = false
+}
 </script>
 
 <template>
     <div class="user-location">
-        <v-btn @click="getUserLocation" variant="outlined"
-            >Use Your Location</v-btn
+        <div
+            v-if="showButtons"
+            class="buttons"
         >
-        <v-btn @click="console.log('test')" variant="outlined"
-            >Enter Location</v-btn
-        >
+            <v-btn
+                @click="getUserLocation"
+                variant="outlined"
+                >Use Your Location</v-btn
+            >
+            <v-btn
+                @click="showEnterLocation"
+                variant="outlined"
+                >Enter Location</v-btn
+            >
+        </div>
+    </div>
+    <div
+        v-if="enterLocation"
+        class="enter-location"
+    >
+        <div class="helper">Enter Your Location:</div>
+        <v-autocomplete
+            :items="city_names"
+            placeholder="Edmonton"
+        ></v-autocomplete>
+        <v-btn>OK</v-btn>
     </div>
 </template>
 
 <style scoped>
 .user-location {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    font-size: 48px;
+    .buttons {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        font-size: 48px;
+    }
+}
+
+.enter-location {
+    width: 300px;
 }
 </style>
