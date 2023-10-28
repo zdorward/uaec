b71280ef68d8a660ce0a2fc5e937ec4206ab0888
Zack Dorward <zdorward1@gmail.com>
2023-10-28 13:59:28 -0600

    made controls

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import UserLocation from './components/UserLocation.vue'
+import Controls from './components/Controls.vue'
 </script>
 
 <template>
     <UserLocation></UserLocation>
+    <Controls
+        city="Edmonton"
+        country="Canada"
+    ></Controls>
 </template>
 
 <style scoped></style>
---
+++ src/components/Controls.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ city: string; country: string }>()
+const range = ref(0)
+const difficulty = ref(0)
+const severity = ref(0)
+</script>
+
+<template>
+    <div class="controls">
+        <div class="slider-container">
+            <div class="slider">
+                <v-slider
+                    v-model="range"
+                    thumb-label
+                    min="0"
+                    max="5"
+                    step="1"
+                ></v-slider>
+            </div>
+            <div class="name">Range of Fire: {{ range }}</div>
+        </div>
+        <div class="slider-container">
+            <div class="slider">
+                <v-slider
+                    thumb-label
+                    min="0"
+                    max="5"
+                    step="1"
+                ></v-slider>
+            </div>
+            <div class="name">Difficulty to contain: {{ difficulty }}</div>
+        </div>
+        <div class="slider-container">
+            <div class="slider">
+                <v-slider
+                    thumb-label
+                    min="0"
+                    max="5"
+                    step="1"
+                ></v-slider>
+            </div>
+            <div class="name">Severity of Fire: {{ severity }}</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.controls {
+    .slider-container {
+        display: flex;
+        .slider {
+            width: 200px;
+        }
+    }
+}
+</style>
